63bc5eb2916ddb527ecbe5ed7d7abdbdb485a8a6
Bakari Sumaila <bakster@users.noreply.github.com>
2014-03-25 11:14:35 +0100

    Update service_ticket_validator.rb
    Use sslv3

--- lib/omniauth/strategies/cas/service_ticket_validator.rb
+++ lib/omniauth/strategies/cas/service_ticket_validator.rb
@@ -78,6 +78,7 @@ module OmniAuth
           result = ''
           http = Net::HTTP.new(@uri.host, @uri.port)
           http.use_ssl = @uri.port == 443 || @uri.instance_of?(URI::HTTPS)
+          http.ssl_version = :SSLv3
           if http.use_ssl?
             http.verify_mode = OpenSSL::SSL::VERIFY_NONE if @options.disable_ssl_verification?
             http.ca_path = @options.ca_path
